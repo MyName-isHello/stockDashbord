92a6e779f47529d09834a19106e8e74c07a0487a
MyName-isHello <wicheung556@gmail.com>
2021-09-15 03:16:58 +0800

    this is tmp branch

--- src/test.py
+++ src/test.py
@@ -2,15 +2,20 @@ import finance as fe
 def main():
 	myac = fe.Account(5000)
 
-	myac.callAgent_buy("tsla", 10, 99, "2021-3-20")
+	myac.callAgent_buy("tsla", 10, 20, "2021-3-20")
 	myac.callAgent_buy("amd", 13, 9, "2021-3-20")
 	myac.callAgent_buy("sq", 1, 984, "2021-3-20")
-	myac.callAgent_buy("tsla", 10, 929, "2021-3-20")
-
+	#myac.callAgent_buy("tsla", 10, 20, "2021-3-20")
+	myac.callAgent_buy("tsla", 10, 10, "2021-3-20")
 	myac.callAgent_sell(myac.Postion[1],9,"2021-4-20",3)
-
-
 	print(myac.showPosition())
+
+	print("#-"*30)
+	#myac.callAgent_sell(trade,sellPrice,sellTime,sellQuantity)
+	myac.callAgent_sell(myac.Postion[1],9,"2021-5-1",1)
+	print(myac.showPosition())
+	print("#-"*30)
+	print(myac.Postion)
 if __name__ == '__main__':
 	main()
 """
